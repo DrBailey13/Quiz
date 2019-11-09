6695433fef4d4484a83a9817e49879121c0c08e6
DeVante Bailey <devantebailey1322@gmail.com>
2019-11-08 19:41:36 -0500

    updated quiz

---
+++ questions.js
@@ -0,0 +1,31 @@
+var questions = [
+    {
+      title: "What symbol is associated with classes?",
+      choices: ["periods", "hashtag", "dollarSign", "equals"],
+      answer: "periods"
+    },
+    {
+      title: "What symbol is associated with Id's?",
+      choices: ["quotes", "hashtag", "parentheses", "semicolon"],
+      answer: "hashtag"
+    },
+    {
+      title: "You access an array element by referring to the?",  
+      choices: ["index number", "array number", "function", "for loop"],
+      answer: "index number"
+    },
+    {
+      title: "which one of these are not an event listener", 
+      choices: ["onclick", "keydown", "eventlisten", "keyup"],
+      answer: "eventlisten" 
+    },
+    {
+       title: "css bootstrap was created on what social media site?",
+       choices: ["twitter", "facebook", "instagram", "snapchat"],
+       answer: "twitter"
+    },
+
+  ];
+
+  
+  
